8d540c081dc4ac82b0cf71121b60b88b944ded31
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-24 13:43:26 +0200

    header done + added gallery pictures

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
+import "./components.css";
+import styled from "@emotion/styled";
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #4BA3C3;
+`;
 
 
 const Header = () => {
     return ( 
         <header>
-
+            <div>
+            <img className="header_img logo" src="/img/pink-me-2.png" alt="Logo" />
+            <img className="header_img" src="/img/navn.png" alt="Name" />
+            </div>
             <nav>
-                <Link to={"/"}>Home</Link>
-                <Link to={"/info"}>Info Page</Link>
-                <Link to={"/gallery"}>Gallery</Link>
+                <StyledLink to={"/"}>Home</StyledLink>
+                <StyledLink to={"/info"}>Info Page</StyledLink>
+                <StyledLink to={"/gallery"}>Gallery</StyledLink>
             </nav>
         </header>
      );
