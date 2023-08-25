3200817a1bfd90f3c7f660996eaca8020c971094
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-25 15:51:40 +0200

    Infopage done + footer made

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
 
+const StyleLink = styled(Link)`
+  color: #4BA3C3;
+  text-decoration: none;
+  line-height: 2rem;
+`;
 
 const Footer = () => {
     return ( 
         <footer>
-            
+            <div>
+            <StyleLink href="https://www.instagram.com/wessltoft/">My personal Instagram</StyleLink>
+            <StyleLink href="https://www.instagram.com/inwesstation/">My art Instagram</StyleLink>
+            </div>
+            <article>
+            <div>
+                <p>Mobile Number:</p>
+                <p> +45 27 83 55 59</p>
+            </div>
+            <div>
+                <p>Email:</p>
+                <p> weslyn-l-toft@gmail.com</p>
+            </div>
+            </article>
         </footer>
      );
 }
--- src/Pages/Info.js
+++ src/Pages/Info.js
@@ -2,10 +2,45 @@
 
 const Info = () => {
     return ( 
-        <section>
-            <h1>This is the Info page</h1>
-            
+        <>
+        <section className="BG_white">
+            <h1>Unleash your Creativity!</h1>
+            <p>To be creative is like a relief for me. For when you hold a lot of emotions inside, that you need to get rid of or express. Art is an amazing tool for me.
+                That being in my art and drawings or in my web developing, doesn’t matter. One part lets me use the creative side of my brain and the other let’s me use the mathematic side of my brain, stimulating my ADHD and letting me work as a normal person.</p>
         </section>
+
+        <section>
+            <h2>A little about me</h2>
+            <p>I will start by admitting that I am dyslexic, however this has never stopped me in completing my tasks or work. Being dyslexic has never really been a highlight in my life, therefore it hasn’t kept me from doing my assignments, as I always find a solution.</p>
+            <p>I work hard and efficiently. I will never stop until I have a solution to a possible problem. I don't just give up and lie down but ask for help or look for it myself to solve the possible solutions.</p>
+            <img className="me_jpg" src="/img/irl-me.jpg" alt="Picture of Weslyn" />
+            <p>I am a person who likes to prettify her body. What this should mean is that I have a lot of tattoos, piercings and even dye my hair, but even though I may look a little scary, I'm a nice and sweet person. I care for my fellow students and coworkers. My style does not define who I am as a person, but instead let’s me express myself in a creative way.</p>
+        </section>
+        
+        <section>
+            <h2>Perks of hiring me</h2>
+            <article className="white_box">
+                <h3>Hard Working</h3>
+                <p>I’m a hardworking individual. I’ve never had it easy in school. This has taught me to work hard for my goal and to succeed. If I discover a problem, I do everything in my power to find a solution. I don’t just give up; I keep looking and I keep working.</p>
+            </article>
+            <article className="white_box">
+                <h3>Efficient</h3>
+                <p>I’m an extremely open-minded person and I’m easy to come to, with a problem. I’m always happy to help others and I enjoy working with others in groups for a project.</p>
+            </article>
+            <article className="white_box">
+                <h3>Creativity</h3>
+                <p>Creativity flows in my blood. You won’t find someone as creative and broad-minded as me. With my ADHD brain I have trouble sitting still, so slacking off won’t be a problem. I must always be working on something, making me a very efficient worker.</p>
+            </article>
+        </section>
+        
+        <section>
+            <h1>Education</h1>
+            <p>- From 2019 to 2021 I took the Danish variant of high school. In high school I took the line called STX, where I studied Spanish A, Danish A, English A and Chinese B. As you properly have noticed, I like learning new languages and to me coding is a language. The reason why I say coding is like learning a new language is because they have a lot of similarities! Learning the words, learning the grama/functions. Learning how to use the grama in a sentence or using the function to create something on the website. I could go on about how similar the two are.</p>
+            <img className="code_png" src="/img/code.png" alt="Picture of code" />
+            <p>- From January 2022 to October 2023, I took the education Web Developer. At this school I have gotten the knowledge for the basics HTML, CSS, JavaScript. Furthermore, I have also been taught about React and the frameworks Remix and Next.js with Tailwind or CSS components. Here I have learned all frontend to become a front-end developer.</p>
+            <p>- From February 2024 to unknown date, I am Studying at Zealand Business School as a Computer Scientist. There I will learn all the backend I need to become a full-stack developer.</p>
+        </section>
+        </>
      );
 }
  
